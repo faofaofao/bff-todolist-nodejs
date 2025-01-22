7aa92396c7861e41069ce90516cd9795a87e2311
Pedro Lavin <pacuevasl@gmail.com>
2025-01-22 01:40:45 -0300

    feat: config local server with API test

--- index.js
+++ index.js
@@ -11,12 +11,24 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
+//API prueba
+app.get('/api',(req, res) => {
+try {
+    res.json({task:'Aprender NOSQL'})
+    res.status(200)
+    console.log('API prueba exitosa')
+} catch (error) {
+    console.error('API prueba falló:',error)    
+}
+})
+
+//Conexión servidor local
 app.listen(PORT, () => {
     try {
-        console.log('Conexion exitosa')
+        console.log(`Conexión exitosa al puerto ${PORT}`)
     } catch (error) {
-        console.error('Error al conectar el servidor', error)
+        console.error('Error al conectar el servidor:', error)
     }
 })
